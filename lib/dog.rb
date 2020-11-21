92f79112cec93622f4260c1ecd7840faabb0a728
elisa <elisaopalka@gmail.com>
2020-11-20 22:21:24 -0500

    commit tree bf83a1154a711b6e089147a1dae3ccce74434410

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Add your code here
+
+class Dog
+    @@all=[]
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        @@all.collect do |name|
+            puts name.name
+        end
+        #  puts @@all.name
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+    
+
+end
